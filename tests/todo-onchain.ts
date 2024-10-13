f6ceb833c82e46360d9c0490de3e7595ad3ea5d2
Ronniery Borges <ronnisti@gmail.com>
2024-10-13 10:33:44 +0100

    feat: :construction: Pushing generated and formated content

--- tests/todo-onchain.ts
+++ tests/todo-onchain.ts
@@ -1,16 +1,16 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { TodoOnchain } from "../target/types/todo_onchain";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { TodoOnchain } from '../target/types/todo_onchain';
 
-describe("todo-onchain", () => {
+describe('todo-onchain', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.TodoOnchain as Program<TodoOnchain>;
 
-  it("Is initialized!", async () => {
+  it('Is initialized!', async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log('Your transaction signature', tx);
   });
 });
